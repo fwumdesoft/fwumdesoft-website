239d4aed73d6f97736ecf9b985dfdaf8085a9da2
Jason R. Carrete <jasoncarrete5@gmail.com>
2017-01-17 22:35:41 -0500

    Changed title to 'fwumdesoft'

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Webpage</title>
+	<title>fwumdesoft</title>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="author" content="Jason R. Carrete" />
