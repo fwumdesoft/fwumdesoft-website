a770492bcf6b7373e0cb4dff3e4dd16163003017
Jason R Carrete <jasoncarrete5@gmail.com>
2017-01-18 21:35:49 +0000

    Added a new line

--- html/home.php
+++ html/home.php
@@ -2,6 +2,7 @@
 <?php
 $games = array_filter(glob('games/*'), 'is_dir');
 foreach($games as $game) {
-    echo "<iframe width=\"100%\" height=\"705\" frameborder=\"0\" scrolling=\"no\" style=\"display:block;\"  src=\"$game/index.html\"></iframe>";
+    echo "<iframe width=\"100%\" height=\"705px\" frameborder=\"0\" scrolling=\"no\"
+        style=\"display:block;\"  src=\"$game/index.html\"></iframe>";
 }
 ?>
