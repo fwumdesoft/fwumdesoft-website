c866fcd3102fb1debcef3ea5daad14e2d2499ac2
Jason R Carrete <jasoncarrete5@gmail.com>
2017-01-18 02:52:55 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Webpage</title>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="author" content="Jason R. Carrete" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script>
+		/* global $ */
+		function loadHome() {
+			$("main").load("html/home.html");
+		}
+		
+		function loadAbout() {
+			$("main").load("html/about.html");
+		}
+	</script>
+</head>
+<body>
+	<div id="page-wrapper">
+		<?php include("html/header.html"); ?>
+
+		<main>
+			<?php include("html/home.html"); ?>
+		</main>
+		
+		<?php include("html/footer.html"); ?>
+	</div>
+</body>
+<link rel="stylesheet" href="css/style.css" />
+</html>
