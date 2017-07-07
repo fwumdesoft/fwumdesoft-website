a5736c1ec4f374316f943f6b756d059eb5a55b75
Jason R. Carrete <jasoncarrete5@gmail.com>
2017-07-07 04:05:17 -0400

    Fixed tab formatting

--- html/games.php
+++ html/games.php
@@ -4,20 +4,20 @@
     <title>Jsonet - Games</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="author" content="Jason R. Carrete">
-		<link rel="stylesheet" href="/css/style.css">
-		<link rel="icon" href="/favicon.png">
+    <meta name="author" content="Jason R. Carrete">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="icon" href="/favicon.png">
   </head>
   <body>
     <header>
-    	<h1>Jsonet <span class="page-qualifier">Games</span></h1>
-    	<nav>
-    		<ul>
-    			<li><a href="/index.html">Home</a></li>
-    			<li><a href="/html/games.php">Web Games</a></li>
-    			<li><a href="/public-files/">Public Files</a></li>
-    		</ul>
-    	</nav>
+      <h1>Jsonet <span class="page-qualifier">Games</span></h1>
+      <nav>
+        <ul>
+          <li><a href="/index.html">Home</a></li>
+          <li><a href="/html/games.php">Web Games</a></li>
+          <li><a href="/public-files/">Public Files</a></li>
+        </ul>
+      </nav>
     </header>
     <main>
       <h2>Games</h2>
@@ -28,7 +28,7 @@
             echo "<iframe width=\"100%\" height=\"705px\" frameborder=\"0\" scrolling=\"no\" style=\"display:block;\" src=\"../games/$gamedir/index.html\"></iframe>";
           }
         }
-      ?>   
+      ?>
     </main>
     <footer>
       <small>&copy; 2017 by Jason R. Carrete</small>
