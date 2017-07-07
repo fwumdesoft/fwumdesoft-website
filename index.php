3f0f64785b281e51418967ed8cd3dd48db0f5de9
Jason R. Carrete <jasoncarrete5@gmail.com>
2017-07-07 01:40:17 -0400

    Formatted head elements properly and modified title

--- index.php
+++ index.php
@@ -1,33 +1,32 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>jsonet</title>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="author" content="Jason R. Carrete" />
-	<link rel="icon" href="favicon.png">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script>
-		/* global $ */
-		function loadHome() {
-			$("main").load("html/home.php");
-		}
-		
-		function loadAbout() {
-			$("main").load("html/about.html");
-		}
-	</script>
-</head>
-<body>
-	<div id="page-wrapper">
-		<?php include("html/header.html"); ?>
+	<head>
+		<title>Jsonet Webserver</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="author" content="Jason R. Carrete">
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="icon" href="favicon.png">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script>
+			function loadHome() {
+				$("main").load("html/home.php");
+			}
+			
+			function loadAbout() {
+				$("main").load("html/about.html");
+			}
+		</script>
+	</head>
+	<body>
+		<div id="page-wrapper">
+			<?php include("html/header.html"); ?>
 
-		<main>
-			<?php include("html/home.php"); ?>
-		</main>
-		
-		<?php include("html/footer.html"); ?>
-	</div>
-</body>
-<link rel="stylesheet" href="css/style.css" />
+			<main>
+				<?php include("html/home.php"); ?>
+			</main>
+			
+			<?php include("html/footer.html"); ?>
+		</div>
+	</body>
 </html>
