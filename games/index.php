852cb6c0b8e83b1afac359b9b567a82e9680602f
Jason Carrete <jasoncarrete5@gmail.com>
2018-07-02 00:03:52 -0400

    Reorganized some html files

--- games/index.php
+++ games/index.php
@@ -11,7 +11,7 @@
   <body>
     <header>
       <h1>Jsonet <span class="page-qualifier">Games</span></h1>
-      <?php include $_SERVER['DOCUMENT_ROOT'].'/html/navbar.html'; ?>
+      <?php include $_SERVER['DOCUMENT_ROOT'] . '/navbar.html'; ?>
     </header>
     <main>
       <h2>Games</h2>
@@ -19,11 +19,11 @@
         $gamesdir = new DirectoryIterator(".");
         foreach ($gamesdir as $gamedir) {
           if (!$gamedir->isDot() && $gamedir->isDir()) {
-            echo "<iframe width=\"100%\" height=\"705px\" frameborder=\"0\" scrolling=\"no\" style=\"display:block;\" src=\"$gamedir/index.html\"></iframe>";
+            echo "<iframe width=\"100%\" height=\"705px\" frameborder=\"0\" scrolling=\"no\" style=\"display:block;\" src=\"$gamedir/\"></iframe>";
           }
         }
       ?>
     </main>
-    <?php include $_SERVER['DOCUMENT_ROOT'].'/html/footer.html'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/footer.html'; ?>
   </body>
 </html>
--- index.php
+++ index.php
@@ -11,13 +11,13 @@
   <body>
     <header>
       <h1>Jsonet <span class="page-qualifier">Home</span></h1>
-      <?php include $_SERVER['DOCUMENT_ROOT'].'/html/navbar.html'; ?>
+      <?php include $_SERVER['DOCUMENT_ROOT'].'/navbar.html'; ?>
     </header>
     <main>
       <section>
         <h2 class="section-header">About this website</h2>
         <p>
-          This webpage is hosted on some old computer made in the early 00s in my basement.
+          This webpage is hosted on some old computer made in the early 2000s in my basement.
           As it approaches the end of its life, I humbly request that you do not DDoS this address (poor computer can
           barely turn on at this point &#9785;). Anyway, this webpage is just a little
           project I've been working on in an attempt to learn more about web development
@@ -25,6 +25,6 @@
         </p>
       </section>
     </main>
-    <?php include $_SERVER['DOCUMENT_ROOT'].'/html/footer.html'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/footer.html'; ?>
   </body>
 </html>
